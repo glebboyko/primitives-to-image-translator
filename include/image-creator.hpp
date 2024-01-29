b34018414fca4984f9fbd4524e810fafa2a0baa5
Gleb Boiko <glebboyko@icloud.com>
2024-01-29 21:01:46 +0300

    Fixed translator

--- include/image-creator.hpp
+++ include/image-creator.hpp
@@ -6,13 +6,13 @@
 #include "primitives.hpp"
 
 template <typename Container, typename Translator>
-concept ImageCreatorReq = requires(Container container) {
-  { Translator(container[std::declval<ssize_t>()][std::declval<ssize_t>()]) };
+concept ImageCreatorReq = requires(Container container, Translator translator) {
+  { translator(container[std::declval<ssize_t>()][std::declval<ssize_t>()]) };
 };
 
 template <typename Container, ImageCreatorReq<Container> Translator>
 void CreateImage(const char* image_file, const Container& image, ssize_t size_x,
-                 ssize_t size_y) {
+                 ssize_t size_y, Translator translator) {
   std::ofstream img_file(image_file);
   if (!img_file.is_open()) {
     throw std::runtime_error("Cannot open file");
@@ -26,7 +26,7 @@ void CreateImage(const char* image_file, const Container& image, ssize_t size_x,
 
   for (ssize_t y = size_y - 1; y >= 0; --y) {
     for (ssize_t x = 0; x < size_x; ++x) {
-      const auto& [red, green, blue] = Translator(image[x][y]);
+      const auto& [red, green, blue] = translator(image[x][y]);
       img_file << red << " " << green << " " << blue << "\n";
     }
   }
