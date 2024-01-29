cb499917c721d583dc5b301398011de1cb7607ba
Gleb Boiko <glebboyko@icloud.com>
2024-01-29 20:56:48 +0300

    Changed image creation principles

--- include/image-creator.hpp
+++ include/image-creator.hpp
@@ -1,22 +1,35 @@
 #pragma once
 
+#include <concepts>
 #include <fstream>
 
 #include "primitives.hpp"
 
-class ImageCreator {
- public:
-  ImageCreator(float px_per_mm, float line_width,
-               const Primitives::Coord& size);
-
-  void Draw(const Primitives::Primitive& primitive);
-
-  void CreateImage(const char* image_name) const;
-
- private:
-  float px_per_mm_;
-  int line_width_;
-  Primitives::Coord size_;
-
-  std::vector<std::vector<bool>> map_;
+template <typename Container, typename Translator>
+concept ImageCreatorReq = requires(Container container) {
+  { Translator(container[std::declval<ssize_t>()][std::declval<ssize_t>()]) };
 };
+
+template <typename Container, ImageCreatorReq<Container> Translator>
+void CreateImage(const char* image_file, const Container& image, ssize_t size_x,
+                 ssize_t size_y) {
+  std::ofstream img_file(image_file);
+  if (!img_file.is_open()) {
+    throw std::runtime_error("Cannot open file");
+  }
+
+  img_file << "P3"
+           << "\n";
+  img_file << size_x << " " << size_y << "\n";
+  img_file << "255"
+           << "\n";
+
+  for (ssize_t y = size_y - 1; y >= 0; --y) {
+    for (ssize_t x = 0; x < size_x; ++x) {
+      const auto& [red, green, blue] = Translator(image[x][y]);
+      img_file << red << " " << green << " " << blue << "\n";
+    }
+  }
+
+  img_file.close();
+}
