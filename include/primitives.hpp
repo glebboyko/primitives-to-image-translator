0123ff7afecd1297e879aaaadd7cbbd7e9ff84f4
glebboyko <115988384+glebboyko@users.noreply.github.com>
2024-02-21 01:03:17 +0300

    Implemented comparison operators for Coord; Changed converted_type in ExtractPrimitives

--- include/primitives.hpp
+++ include/primitives.hpp
@@ -14,6 +14,17 @@ struct Coord {
 };
 
 bool operator==(const Coord&, const Coord&) noexcept;
+bool operator!=(const Coord& first, const Coord& second) noexcept;
+bool operator<(const Coord& first, const Coord& second) noexcept;
+bool operator<=(const Coord& first, const Coord& second) noexcept;
+bool operator>(const Coord& first, const Coord& second) noexcept;
+bool operator>=(const Coord& first, const Coord& second) noexcept;
+
+const double kPi = 3.1415926535;
+const int kDegInCircle = 360;
+
+double DegToRad(double deg);
+double RadToDeg(double rad);
 
 double GetDistance(const Coord& first, const Coord& second) noexcept;
 
@@ -42,19 +53,18 @@ class Segment : public Primitive {
 };
 
 std::list<Segment> BaseExtractPrimitives(
-    std::vector<std::vector<std::pair<bool, int64_t>>>& bitmap);
+    std::vector<std::vector<bool>>& bitmap);
 
 template <typename Container, typename Translator>
   requires ImageBitmap<Container> &&
            AvailabilityTranslator<Container, Translator>
 std::list<Segment> ExtractPrimitives(const Container& container, int size_x,
                                      int size_y, Translator translator) {
-  std::vector<std::vector<std::pair<bool, int64_t>>> converted_bitmap(
-      size_x, std::vector<std::pair<bool, int64_t>>(size_y));
+  std::vector<std::vector<bool>> converted_bitmap(size_x,
+                                                  std::vector<bool>(size_y));
   for (int x = 0; x < size_x; ++x) {
     for (int y = 0; y < size_y; ++y) {
-      converted_bitmap[x][y] = {static_cast<bool>(translator(container[x][y])),
-                                INT64_MAX};
+      converted_bitmap[x][y] = static_cast<bool>(translator(container[x][y]));
     }
   }
 
