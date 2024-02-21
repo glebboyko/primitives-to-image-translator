da24cbec075e0e6d08333cdcc58a763ca7a25afd
glebboyko <115988384+glebboyko@users.noreply.github.com>
2024-02-22 02:10:31 +0300

    Implemented Segment methods (GetAngle, SetLen, SetAngle, GetArea)

--- include/primitives.hpp
+++ include/primitives.hpp
@@ -36,14 +36,21 @@ class Segment : public Primitive {
   Coord& GetB();
   const Coord& GetA() const;
   const Coord& GetB() const;
+  double GetAngle() const;
+
+  void SetLen(int len);
+  void SetAngle(double deg);
 
   std::list<Coord> GetGraphic() const override;
+  std::list<Coord> GetArea(int radius) const;
 
  private:
   Coord a_point_;
   Coord b_point_;
 
   int GetBCoefficient() const;
+  Coord GetCenter() const;
+  void SetKCoef(double new_k);
 };
 
 class Circe : public Primitive {
--- source/primitives.cpp
+++ source/primitives.cpp
@@ -50,6 +50,33 @@ Coord& Segment::GetB() { return b_point_; }
 const Coord& Segment::GetA() const { return a_point_; }
 const Coord& Segment::GetB() const { return b_point_; }
 
+double Segment::GetAngle() const {
+  auto k_coef = GetKCoefficient(*this);
+  return k_coef == FLT_MAX ? kDegInCircle / 4 : atan(k_coef);
+}
+
+void Segment::SetLen(int len) {
+  auto center = GetCenter();
+  if (a_point_.x == b_point_.x) {
+    a_point_.y = center.y - len / 2;
+    b_point_.y = center.y + (len / 2) + (len % 2);
+    return;
+  }
+
+  double a_len_ratio = len / GetDistance(center, a_point_);
+  double b_len_ratio = len / GetDistance(center, b_point_);
+
+  a_point_ = {
+      static_cast<int>(a_len_ratio * (a_point_.x - center.x) + center.x),
+      static_cast<int>(a_len_ratio * (a_point_.y - center.y) + center.y)};
+
+  b_point_ = {
+      static_cast<int>(b_len_ratio * (b_point_.x - center.x) + center.x),
+      static_cast<int>(b_len_ratio * (b_point_.y - center.y) + center.y)};
+}
+
+void Segment::SetAngle(double deg) { SetKCoef(tan(DegToRad(deg))); }
+
 std::list<Coord> Segment::GetGraphic() const {
   Segment normalized(a_point_, b_point_);
   if (a_point_ > b_point_) {
@@ -91,6 +118,67 @@ int Segment::GetBCoefficient() const {
   return ((static_cast<int64_t>(b_point_.x) * a_point_.y) -
           (static_cast<int64_t>(a_point_.x) * b_point_.y)) /
          (b_point_.x - a_point_.x);
+}
+
+Coord Segment::GetCenter() const {
+  return {(a_point_.x + b_point_.x) / 2, (a_point_.y + b_point_.y) / 2};
+}
+
+void Segment::SetKCoef(double new_k) {
+  auto center = GetCenter();
+  double c_len = GetDistance(a_point_, b_point_) / 2;
+
+  double sin_n;
+  double cos_n;
+
+  if (new_k == FLT_MAX) {
+    sin_n = 1;
+    cos_n = 0;
+  } else {
+    double denominator = sqrt(1 + (new_k * new_k));
+
+    sin_n = new_k / denominator;
+    cos_n = 1 / denominator;
+  }
+
+  int delta_x = c_len * cos_n;
+  int delta_y = c_len * sin_n;
+
+  a_point_ = {center.x - delta_x, center.y - delta_y};
+  b_point_ = {center.x + delta_x, center.y + delta_y};
+}
+
+std::list<Coord> Segment::GetArea(int radius) const {
+  std::list<Coord> area;
+
+  area.splice(area.cend(), Circe(a_point_, radius).GetGraphic());
+  area.splice(area.cend(), Circe(b_point_, radius).GetGraphic());
+
+  auto init_k = GetKCoefficient(*this);
+  float norm_k;
+  if (init_k == 0) {
+    norm_k = FLT_MAX;
+  } else if (init_k == FLT_MAX) {
+    norm_k = 0;
+  } else {
+    norm_k = - (1 / init_k);
+  }
+
+  Segment from_a({a_point_, a_point_});
+  from_a.SetLen(radius * 2);
+  from_a.SetKCoef(norm_k);
+
+  Segment from_b({b_point_, b_point_});
+  from_b.SetLen(radius * 2);
+  from_b.SetKCoef(norm_k);
+
+  Segment upper_bound(from_a.b_point_, from_b.b_point_);
+  Segment lower_bound(from_a.a_point_, from_b.a_point_);
+
+  area.splice(area.cend(), upper_bound.GetGraphic());
+  area.splice(area.cend(), lower_bound.GetGraphic());
+
+  return FulfillArea(area);
 }
 
 /*---------------------------------- circle ----------------------------------*/
